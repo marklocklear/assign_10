1584c70b9b557784d5ac39a5221d9b94f0737c13
Charlie Wallin <charliekwallin@gmail.com>
2019-03-30 11:22:51 -0400

    Tweaked the title to asgn10. Code is ready for students to use for asgn10

--- index.view.php
+++ index.view.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>asgn09</title>
+    <title>asgn010</title>
 </head>
 <body>
 
