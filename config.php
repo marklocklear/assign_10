0658dc25c380c8c4232ec2aa7c9edbfdadd154f9
Charlie Wallin <charliekwallin@gmail.com>
2019-04-01 08:50:31 -0400

    The config.php file is almost complete. Need to add the options array

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'database'  => [
+        'name'      =>  'mytodo',
+        'username'  =>  'root',
+        'pass'      =>  'root',
+        'connection'=>  'mysql:host=127.0.0.1:8889',
+        'options'   =>  []
+    ]
+];
--- database/Connection.php
+++ database/Connection.php
@@ -2,10 +2,17 @@
 
 class Connection
 {
-    public static function make()
+    public static function make($config)
     {
         try {
-            return new PDO('mysql:host=127.0.0.1:8889; dbname=mytodo', 'root', 'root' );
+            //return new PDO('mysql:host=127.0.0.1:8889; dbname=mytodo', 'root', 'root' );
+            return new PDO(
+                $config['connection'] . ';dbname=' . $config['name'],
+                $config['username'], 
+                $config['pass'],
+                $options['options']
+            );
+
         } catch (PDOException $e) {
             die('Could not connect');
         }
